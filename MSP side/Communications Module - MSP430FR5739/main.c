072010948cccff5be35628be1d4c473df32162a6
James Sutton <j.sutton@ceresmg.com>
2012-06-28 19:09:56 -0300

    added queue functionality
    next is the RMA for an unlimited queue

--- MSP side/Communications Module - MSP430FR5739/main.c
+++ MSP side/Communications Module - MSP430FR5739/main.c
@@ -1,7 +1,9 @@
 #include <msp430fr5739.h>
+
 #include <stdlib.h>
-#include <cstring>
+#include "queue.h"
 //define bits as MCU pins
+
 #define STEP_A	BIT0
 #define STEP_B	BIT1
 #define DIR_A	BIT2
@@ -14,55 +16,117 @@
 #define SLEEP	BIT4
 #define RESET	BIT5
 
+
+
+
+
 void ConfigEverything(void);
-void putchar(unsigned int c);
-int getchar(void);
-void ConfigWDT(void);
-void ResetMotors(void);
+void putbyte(unsigned int c);
+int getbyte(void);
+void ConfigWDT(void);	// also actually use the WDT when you move off the shitty fraunchpad
+
+//actually do something with error handling
 void ErrorHandling(int er_num);
+
+
+//initalizing the mechanical peripherals
+void ResetMotors(void);
+void LaserWarmup(void);
+
+
+
+// the motor stuff, do it later
+void MotorX(int dat);
+void MotorY(int dat);
+void LaserOff(void);
+void LaserOn(void);
+
+
 void main(void)
 {
 	ConfigEverything();
 	ConfigWDT();
-	int error=0;
-	__bis_SR_register(LPM3_bits + GIE);
-	_no_operation();
-	while(!error)
+
+	int end=0; // function cease command
+	Queue Qx,Qy;
+
+
+
+
+	//configure data handler
+	ResetMotors();
+	LaserWarmup();
+	putbyte(0xff); // all clear
+	int n=0;
+	while(1)
 	{
+		end=getbyte(); // gets a 0 from the computer, unless its the end, then its a 1
+		if(end)
+		break;
+		int StackSize=getbyte();	//creates the queue to the size specified,
+		Qx= CreateQueue(StackSize);	// will set perminently later
+		Qy= CreateQueue(StackSize);	//
+
+		while(!(IsFull(Qx) || IsFull(Qy))) // while queue is not full, fill er up
+		{
+			n++;
+			int buff=getbyte();	// get buffer byte from PC
+			if(n&1)
+				Enqueue(buff,Qx);	//put buffer byte in queue x
+			else
+				Enqueue(buff,Qy);// put buffer byte in queue y
+
+		}
 
 
 
+		putbyte(0xff);	// all clear, waiting for start confirmation
 
 
 
+		while(!(IsEmpty(Qy) || IsEmpty(Qx)))
+		{
+			LaserOn();
+			MotorX(FrontAndDequeue(Qx));
+			MotorY(FrontAndDequeue(Qy));
 
-
-
-		ResetMotors();
+		}
+		LaserOff();
 
 	}
-	}
 
-
-
-void ConfigWDT(void)
-{
-	WDTCTL = WDTPW + WDTHOLD;
- }
-
-
-#pragma vector=PORT4_VECTOR
-__interrupt void Port_4(void)
-{
-	ErrorHandling(1);
-	getchar();
-	P4IFG &=~(BIT0);
-	__bic_SR_register_on_exit(LPM3_bits);
 }
 
 
 
 
+
+
+
+
+
+
+	void ConfigWDT(void)
+	{
+		WDTCTL = WDTPW + WDTHOLD;
+	 }
+
+
+	#pragma vector=PORT4_VECTOR
+	__interrupt void BUTTON(void)		//start on button interrupt
+	{
+		P4IFG &=~(BIT0);
+		__bic_SR_register_on_exit(LPM3_bits);
+	}
+
+
+
+	#pragma vector=PORT3_VECTOR
+	__interrupt void PORT3(void)		//kill if error catch is bad
+	{
+		__bis_SR_register(LPM4_bits);
+		_no_operation();
+}
 
 
 
@@ -78,15 +142,15 @@ __interrupt void Port_4(void)
 void ConfigEverything(void)
 {
 
-	//configure clocks
-	CSCTL0_H = 0xA5; // Unlock register
+		//configure clocks
+		CSCTL0_H = 0xA5; // Unlock register
 	  CSCTL1 |= DCOFSEL0 + DCOFSEL1; // Set max. DCO setting
 	  CSCTL2 = SELS_3 + SELM_3; // MCLK = DCO
 	  CSCTL3 =  DIVS_0 + DIVM_0; // set all dividers
 	  CSCTL0_H = 0x01; // Lock Register
 
 
-	//configure MCU pins
+	  //configure MCU pins
 	  P1DIR|=STEP_A+STEP_B+DIR_A+DIR_B+MS1_A+MS1_B+MS2_A+MS2_B;
 	  P2DIR|=ENABLE+SLEEP+RESET;
 
@@ -103,20 +167,21 @@ void ConfigEverything(void)
 
 
 
-	  //configure button pins for interrupt enable
+	  //configure button pins for initial interrupt enable
 	  P4DIR &=~(BIT0);
 	  P4IE|=BIT0;
 
-
-
+	  // configure P3 pins for generic error message interrupt
+	  P3DIR &=~(BIT0);
+	  P3IE|=BIT0;
 
 
 
 
 	//configure UART pins
+	P2DIR |= BIT0;
 	P2SEL1 |= BIT0 + BIT1;
 	P2SEL0 &= ~(BIT0+BIT1);
-
 
 
 	//configure UART
@@ -134,13 +199,13 @@ void ConfigEverything(void)
 
 void ErrorHandling(int er_num)
 {
-		putchar(0);
-		putchar(er_num);
-		putchar(0);
+	putbyte(er_num);
+	P3IFG |=BIT0;
+
 }
 
 
-void putchar(unsigned int c)
+void putbyte(unsigned int c)
 {
     while(!(UCA0IFG & UCTXIFG));
     UCA0TXBUF = c;
@@ -148,19 +213,46 @@ void putchar(unsigned int c)
 }
 
 
-int getchar(void)
+
+int getbyte(void)
 {
-    while(!(UCA0IFG & UCRXIFG));
+    while(!(UCA0IFG & UCSTTIFG));
+    _delay_cycles(50000);
     const int c = UCA0RXBUF;
-    UCA0IFG &= ~UCRXIFG;
+    UCA0IFG &= ~(UCRXIFG+UCSTTIFG);
      return c;
 }
 
 
 void ResetMotors(void)
 {
-	P2OUT&= ~SLEEP;								//turn on MCU
+	P2OUT&=~SLEEP;								//turn on MCU
 	P2OUT |= RESET;								// reset location of MCU
 	  _delay_cycles(50);						// wait for reset
 	  P2OUT &= ~(RESET);						// turn off reset bit
+	  P2OUT|=SLEEP;
+}
+void LaserWarmup(void)// Fill all these later when I have proper pins to use
+{
+	_delay_cycles(50);
+}
+
+void LaserOff(void)
+{
+
+}
+
+void LaserOn(void)	//
+{
+
+}
+
+void MotorX(int dat)//
+{
+
+}
+
+void MotorY(int dat)//
+{
+
 }
